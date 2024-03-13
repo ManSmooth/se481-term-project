48a1d700c11646c1427470bc52c8fb8abc1760c2
ManSmooth <40175264+ManSmooth@users.noreply.github.com>
2024-03-14 00:51:11 +0700

    suggestion

--- food_website/src/lib/elasticsearch.ts
+++ food_website/src/lib/elasticsearch.ts
@@ -1,6 +1,7 @@
 export interface SearchResult {
 	elapse: number;
 	results: Recipe[];
+	suggest: string;
 	status: string;
 }
 
--- food_website/src/routes/search/+page.ts
+++ food_website/src/routes/search/+page.ts
@@ -1,9 +1,15 @@
 import type { Recipe, SearchResult } from '$lib';
-import { page } from '$app/stores'
+import { page } from '$app/stores';
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, url }) {
-	const res = await fetch(`http://34.126.162.255:5000/search?query=${url.searchParams.get("query")}`);
+	const res = await fetch(
+		`http://34.126.162.255:5000/search?query=${url.searchParams.get('query')}`
+	);
 	const result: SearchResult = await res.json();
+	const suggest_res = await fetch(
+		`http://34.126.162.255:5000/suggest?query=${url.searchParams.get('query')}`
+	);
+	const suggest_result: SearchResult = await suggest_res.json();
 
-	return { result };
+	return { result, suggest_result, query: url.searchParams.get('query') };
 }
